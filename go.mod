d52ef2d368cd2456be7eab4556a2160707f1a5c0
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2023-03-08 21:58:54 +0530

    add benchmark for xxhash and native go map
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/utkarsh-pro/godc
 
 go 1.20
+
+require github.com/cespare/xxhash v1.1.0 // indirect
--- internal/bench/bench_test.go
+++ internal/bench/bench_test.go
@@ -1,9 +1,13 @@
 package bench
 
 import (
+	"crypto/rand"
+	"hash/maphash"
 	"sync"
 	"sync/atomic"
 	"testing"
+
+	"github.com/cespare/xxhash"
 )
 
 func BenchmarkInts(b *testing.B) {
@@ -36,6 +40,66 @@ func BenchmarkInts(b *testing.B) {
 			}
 			i++
 			mu.Unlock()
+		}
+	})
+}
+
+func BenchmarkHashing(b *testing.B) {
+	b.ReportAllocs()
+
+	b.Run("native", func(b *testing.B) {
+		set := map[string]int{
+			"4B":   4,
+			"16B":  16,
+			"100B": 100,
+			"4KB":  4096,
+			"10MB": 10 * 1e6,
+		}
+
+		for k, val := range set {
+			b.Run(k, func(b *testing.B) {
+				b.SetBytes(int64(val))
+
+				byts := make([]byte, val)
+				if _, err := rand.Read(byts); err != nil {
+					b.Fatal(err)
+				}
+
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					var hasher maphash.Hash
+					hasher.Write(byts)
+					_ = hasher.Sum64()
+				}
+			})
+		}
+	})
+
+	b.Run("xxhash", func(b *testing.B) {
+		set := map[string]int{
+			"4B":   4,
+			"16B":  16,
+			"100B": 100,
+			"4KB":  4096,
+			"10MB": 10 * 1e6,
+		}
+
+		for k, val := range set {
+			b.Run(k, func(b *testing.B) {
+				b.SetBytes(int64(val))
+
+				byts := make([]byte, val)
+				if _, err := rand.Read(byts); err != nil {
+					b.Fatal(err)
+				}
+
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					hasher := xxhash.New()
+					hasher.Write(byts)
+					_ = hasher.Sum64()
+				}
+			})
 		}
 	})
 }
